4f846870e26a5369e2c6d8e3767abcbfff96c7b7
Evandro <evandrovogel988@gmail.com>
2022-01-25 21:14:16 -0300

    Primeiro Commit

---
+++ Teste.php
@@ -0,0 +1,15 @@
+<?php
+
+class Teste
+{
+    static function function_name() {
+        echo 'teste';
+    }
+}
+
+class Teste2
+{
+    static function function_name2() {
+        echo 'teste2';
+    }
+}
